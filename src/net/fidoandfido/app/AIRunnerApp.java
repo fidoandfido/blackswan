b94654846888e705b32591cf30a7394e2a9a4f5f
Andy K <fidoandfido@gmail.com>
2011-05-08 22:09:58 +1000

    AI Runner App. - just like the thread, only do it once.

---
+++ src/net/fidoandfido/app/AIRunnerApp.java
@@ -0,0 +1,18 @@
+package net.fidoandfido.app;
+
+import net.fidoandfido.dao.HibernateUtil;
+import net.fidoandfido.engine.ai.AIRunner;
+
+public class AIRunnerApp {
+
+	public static void main(String[] args) {
+		HibernateUtil.connectToDB();
+		HibernateUtil.beginTransaction();
+
+		AIRunner runner = new AIRunner();
+		runner.process();
+
+		HibernateUtil.commitTransaction();
+
+	}
+}
