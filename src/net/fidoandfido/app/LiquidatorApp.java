cc857d872ea58421156a5246a7fd36818c0ebce3
Andy K <fidoandfido@gmail.com>
2011-05-18 23:12:11 +1000

    Liquidating AI implemented for AIs to liquidate their holdings.

---
+++ src/net/fidoandfido/app/LiquidatorApp.java
@@ -0,0 +1,50 @@
+package net.fidoandfido.app;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import net.fidoandfido.dao.HibernateUtil;
+import net.fidoandfido.dao.OrderDAO;
+import net.fidoandfido.dao.TraderDAO;
+import net.fidoandfido.engine.ai.AITrader;
+import net.fidoandfido.engine.ai.LiquididatingAI;
+import net.fidoandfido.model.Order;
+import net.fidoandfido.model.Trader;
+
+public class LiquidatorApp {
+
+	public static void main(String argv[]) {
+		HibernateUtil.connectToDB();
+		LiquidatorApp app = new LiquidatorApp();
+		app.liquidate("fred6");
+	}
+
+	Logger logger = Logger.getLogger(this.getClass().getName());
+	private TraderDAO traderDAO;
+
+	public LiquidatorApp() {
+		// Nothing here!
+		traderDAO = new TraderDAO();
+	}
+
+	public void liquidate(String traderName) {
+		HibernateUtil.beginTransaction();
+
+		Trader trader = traderDAO.getTraderByName(traderName);
+		List<Order> closedOrders = OrderDAO.getOpenOrdersByTrader(trader);
+		if (closedOrders.size() > 0) {
+			logger.info("AIRunner - removing outstanding orders: " + trader.getName());
+		}
+		for (Order order : closedOrders) {
+			OrderDAO.deleteOrder(order);
+		}
+
+		// Liquidate!
+		AITrader aiTrader = new LiquididatingAI();
+		aiTrader.performTrades(trader);
+		trader.setCash(AppInitialiser.TRADER_START_CASH * 10);
+		traderDAO.saveTrader(trader);
+		HibernateUtil.commitTransaction();
+	}
+
+}
---
+++ src/net/fidoandfido/engine/ai/LiquididatingAI.java
@@ -0,0 +1,56 @@
+package net.fidoandfido.engine.ai;
+
+import java.util.List;
+
+import net.fidoandfido.dao.OrderDAO;
+import net.fidoandfido.dao.ShareParcelDAO;
+import net.fidoandfido.model.Company;
+import net.fidoandfido.model.Order;
+import net.fidoandfido.model.ShareParcel;
+import net.fidoandfido.model.StockExchange;
+import net.fidoandfido.model.Trader;
+
+public class LiquididatingAI extends AITrader implements AITradeStrategy {
+
+	public static final String NAME = "Liquidating";
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public void performTrades(Trader trader) {
+		ShareParcelDAO shareParcelDAO = new ShareParcelDAO();
+		OrderDAO orderDAO = new OrderDAO();
+		Iterable<ShareParcel> holdings = shareParcelDAO.getHoldingsByTrader(trader);
+		for (ShareParcel shareParcel : holdings) {
+			// We are going to sell all our shares!
+			long shareCount = shareParcel.getShareCount();
+			Company company = shareParcel.getCompany();
+			long askingPrice = company.getLastTradePrice();
+			// Since we are selling, drop the price by 5 %
+			long bigDelta = (askingPrice * 5);
+			long bigAskingPrice = (100 * askingPrice) + bigDelta;
+			askingPrice = bigAskingPrice / 100;
+			if (askingPrice == 0) {
+				askingPrice = 1;
+			}
+			// Check if we have any open sell order for this company...
+			List<Order> openOrders = OrderDAO.getOpenOrdersByTrader(trader, company);
+			for (Order order : openOrders) {
+				order.setActive(false);
+				orderDAO.saveOrder(order);
+			}
+			logger.info("ai: " + trader.getAiStrategyName() + "(" + trader.getName() + ") sells " + shareCount + " of " + company.getName() + " at "
+					+ askingPrice);
+			Order sellOrder = new Order(trader, company, shareCount, askingPrice, Order.OrderType.SELL);
+			orderDAO.saveOrder(sellOrder);
+
+			// Attempt to process the order...
+			StockExchange exchange = company.getStockExchange();
+			exchange.processOrder(sellOrder);
+		}
+
+	}
+}
